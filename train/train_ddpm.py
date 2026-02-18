a88ec3f3aa8fd3fe00496b6e5d4d982931ed0529
Shani Lavy <shanilavy1@mail.tau.ac.il>
2026-02-18 11:16:22 +0200

    train ddpm

--- train/train_ddpm.py
+++ train/train_ddpm.py
@@ -1,4 +1,4 @@
-from ddpm.diffusion import GaussianDiffusion, Trainer, Unet3D
+from ddpm.diffusion import GaussianDiffusion, Trainer
 import hydra
 from omegaconf import DictConfig, OmegaConf, open_dict
 from train.get_dataset import get_dataset
@@ -15,46 +15,33 @@ def run(cfg: DictConfig):
         cfg.model.results_folder = os.path.join(
             cfg.model.results_folder, cfg.dataset.name, cfg.model.results_folder_postfix)
 
+    # Create 2D UNet for X-ray diffusion with CT conditioning
+    model = UNet(
+        in_ch=cfg.model.diffusion_num_channels,  # 1 for grayscale X-ray
+        out_ch=cfg.model.diffusion_num_channels,  # 1 for noise prediction
+        spatial_dims=2,  # 2D UNet for X-ray
+    ).cuda()
 
-    if cfg.model.denoising_fn == 'Unet3D':
-        model = Unet3D(
-            dim=cfg.model.diffusion_img_size,
-            cond_dim=cfg.model.cond_dim,
-            dim_mults=cfg.model.dim_mults,
-            channels=cfg.model.diffusion_num_channels,
-            resnet_groups=8,
-            classifier_free_guidance = cfg.model.classifier_free_guidance,
-            medclip = cfg.model.medclip
-        ).cuda()
-    elif cfg.model.denoising_fn == 'UNet':
-        model = UNet(
-            in_ch=cfg.model.diffusion_num_channels,
-            out_ch=cfg.model.diffusion_num_channels,
-            spatial_dims=2 #changed from 3 to 2
-        ).cuda()
-    else:
-        raise ValueError(f"Model {cfg.model.denoising_fn} doesn't exist")
-
-    # Create diffusion model
+    # Create diffusion model for 2D X-ray generation conditioned on CT
     diffusion = GaussianDiffusion(
         model,
         vqgan_ckpt=cfg.model.vqgan_ckpt,
         vae_ckpt=cfg.model.vae_ckpt,
-        image_size=cfg.model.diffusion_img_size,
+        image_size=cfg.model.diffusion_img_size, # 224
         num_frames=cfg.model.diffusion_depth_size,
-        channels=cfg.model.diffusion_num_channels,
+        channels=cfg.model.diffusion_num_channels, # 1
         timesteps=cfg.model.timesteps,
-        img_cond = True,
+        img_cond=True,
         loss_type=cfg.model.loss_type,
-        l1_weight = cfg.model.l1_weight,
-        perceptual_weight = cfg.model.perceptual_weight,
-        discriminator_weight = cfg.model.discriminator_weight,
-        classification_weight = cfg.model.classification_weight,
-        classifier_free_guidance = cfg.model.classifier_free_guidance,
-        medclip = cfg.model.medclip,
-        name_dataset = cfg.model.name_dataset,
-        dataset_min_value = cfg.model.dataset_min_value,
-        dataset_max_value = cfg.model.dataset_max_value,
+        l1_weight=cfg.model.l1_weight,
+        perceptual_weight=cfg.model.perceptual_weight,
+        discriminator_weight=cfg.model.discriminator_weight,
+        classification_weight=cfg.model.classification_weight,
+        classifier_free_guidance=cfg.model.classifier_free_guidance,
+        medclip=cfg.model.medclip,
+        name_dataset=cfg.model.name_dataset,
+        dataset_min_value=cfg.model.dataset_min_value,
+        dataset_max_value=cfg.model.dataset_max_value,
     ).cuda()
 
     # Get datasets
